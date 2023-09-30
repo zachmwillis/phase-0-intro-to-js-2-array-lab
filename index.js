a301474c1f7f1b6c0c856d5e39796b5eaacb4138
Zach Willis <zachmw27@gmail.com>
2023-09-30 19:02:58 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo" , "Otis" , "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.push(name);
+    return copyOfCats;
+}
+
+function prependCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.unshift(name)
+    return copyOfCats;
+}
+
+function removeLastCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.pop();
+    return copyOfCats;
+}
+
+function removeFirstCat(name) {
+    const copyOfCats = [...cats];
+    copyOfCats.shift();
+    return copyOfCats;
+}
